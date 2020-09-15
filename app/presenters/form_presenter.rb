0ce1b9c74597c45edfd7db5e91592a00b7554fb1
ryo27-jp <calvee.20@gmail.com>
2020-09-15 13:09:33 +0000

    finshChapter16

--- app/presenters/form_presenter.rb
+++ app/presenters/form_presenter.rb
@@ -1,3 +1,4 @@
+
 class FormPresenter
   include HtmlBuilder
 
@@ -37,6 +38,14 @@ class FormPresenter
     markup(:div, class: "input-block") do |m|
       m << decorated_label(name, label_text, options)
       m << date_field(name, options)
+      m << error_messages_for(name)
+    end
+  end
+
+  def drop_down_list_block(name, label_text, choices, options = {})
+    markup(:div, class: "input-block") do |m|
+      m << decorated_label(name, label_text, options)
+      m << form_builder.select(name, choices, { include_blank: true }, options)
       m << error_messages_for(name)
     end
   end
